ce9d07b82fc7bb622a68ed89e9d1cf4470274f4a
michelsciortino <michel.sciortino@outlook.com>
2019-10-29 22:21:18 +0100

    Adding /bin/sleep to  command + cleaning code

--- CI/extract_from_docker_image.sh
+++ CI/extract_from_docker_image.sh
@@ -5,22 +5,28 @@ then
     container=$1
 fi
 # Copying binaries
+echo "Copying $container:/usr/local/bin to /usr/local"
 sudo docker cp -a $container:/usr/local/bin /usr/local
 # Copying headers
+echo "Copying $container:/usr/include/polycube to /usr/include"
 sudo mkdir -p /usr/include/polycube
 sudo docker cp -a $container:/usr/include/polycube /usr/include
 # Copying libraries
 for lib in $(docker exec $container ls /usr/lib | grep "libp"); do
+    echo "Copying $container:/usr/lib/$lib to /usr/lib/"
     sudo docker cp $container:/usr/lib/$lib /usr/lib/
 done
 ldconfig -n /usr/lib
 for lib in $(docker exec $container ls /usr/local/lib | grep "lib"); do
+    echo "Copying $container:/usr/local/lib/$lib to /usr/local"
     sudo docker cp $container:/usr/local/lib/$lib /usr/local
 done
 ldconfig -n /usr/local/lib
 for lib in $(docker exec $container ls /usr/lib/x86_64-linux-gnu/ | grep -P "libyang|libnl"); do
+    echo "Copying $container:/usr/lib/x86_64-linux-gnu/$lib to /usr/lib/x86_64-linux-gnu"
     sudo docker cp $container:/usr/lib/x86_64-linux-gnu/$lib /usr/lib/x86_64-linux-gnu
 done
 for lib in $(docker exec $container ls /lib/x86_64-linux-gnu/ | grep -P "libnl"); do
+    echo "Copying $container:/lib/x86_64-linux-gnu/$lib to /lib/x86_64-linux-gnu"
     sudo docker cp $container:/lib/x86_64-linux-gnu/$lib /lib/x86_64-linux-gnu
 done
