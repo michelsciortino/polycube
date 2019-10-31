622d8b38bdfbb7d3eb7355b86cc38e19854e23c0
michelsciortino <michel.sciortino@outlook.com>
2019-10-31 14:23:47 +0100

    Adding -rf to rm commands

--- CI/clean_slave.sh
+++ CI/clean_slave.sh
@@ -15,18 +15,18 @@ sudo rm -rf /usr/include/polycube
 echo "# Removing libraries"
 for lib in $(ls /usr/lib | grep "libp"); do
     echo "removing /usr/lib/$lib"
-    sudo rm /usr/lib/$lib
+    sudo rm -rf /usr/lib/$lib
 done
 for lib in $(ls /usr/local/lib | grep "lib"); do
     echo "removing /usr/local/lib/$lib"
-    sudo rm /usr/local/lib/$lib
+    sudo rm -rf /usr/local/lib/$lib
 done
 for lib in $(ls /usr/lib/x86_64-linux-gnu/ | grep -P "libyang|libnl"); do
     echo "removing /usr/lib/x86_64-linux-gnu/$lib"
-    sudo rm /usr/lib/x86_64-linux-gnu/$lib
+    sudo rm -rf /usr/lib/x86_64-linux-gnu/$lib
 done
 for lib in $(ls /lib/x86_64-linux-gnu/ | grep -P "libnl"); do
     echo "removing /lib/x86_64-linux-gnu/$lib"
-    sudo rm /lib/x86_64-linux-gnu/$lib
+    sudo rm -rf /lib/x86_64-linux-gnu/$lib
 done
 sudo ldconfig
